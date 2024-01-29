417aac915d6f936001170b417d4552bc1e8452d9
Wafffel <you@example.comkristoferski.kw@gmail.com>
2024-01-29 22:11:44 +0100

    add configuration

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"Firts verison. Working only for specific devices." 
+Simple app to use silver pens fastly and without any effort.
+
+How to use:
+First set location of silver pens and place for a signature by moving mouse cursor over it and clicking right key.
+Default locations should work when game is on fullscreen on 1920x1080 screen (not tested)
+Then click start key and enjoy saved time.
+
+Controls:
+f1 - set point where silver pens are on located
+f2 - set point where place for a signature is on located
+f4 - start/stop
+esc - close app
--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import time
 import pyautogui as pgui
 from pynput import keyboard
-import sys
 
 action_duration = 0.1
 start_duration = 1
 pen_location = {'x':200, 'y':850}
-refresh_location = {'x':200, 'y':770}
-move_to_pen_durtion = 1
+signature_location = {'x':200, 'y':770}
 close_key = keyboard.Key.esc
+stop_start_key = keyboard.Key.f4
+set_pen_location_key = keyboard.Key.f1
+set_signature_location_key = keyboard.Key.f2
 
-pgui.moveTo(pen_location['x'], pen_location['y'], move_to_pen_durtion)
-
+is_running = False
 exit_program = False
 def on_press(key):
     global exit_program
+    global is_running
     if key == close_key:
         exit_program = True 
         return False
+    if key == set_pen_location_key:
+        pen_location['x'], pen_location['y'] = pgui.position()
+    if key == set_signature_location_key:
+        signature_location['x'], signature_location['y'] = pgui.position()
+    if key == stop_start_key:
+        is_running = not is_running
+        
 with keyboard.Listener(on_press) as listener:
     time.sleep(start_duration)
     while exit_program == False:
-        pgui.click(duration=action_duration)
-        pgui.moveTo(refresh_location['x'], refresh_location['y'], action_duration)
-        pgui.click(duration=action_duration)
-        pgui.click(duration=action_duration)
-        pgui.moveTo(pen_location['x'], pen_location['y'], action_duration)
+        if is_running:
+            pgui.moveTo(pen_location['x'], pen_location['y'], action_duration)
+            pgui.click(duration=action_duration)
+            pgui.moveTo(signature_location['x'], signature_location['y'], action_duration)
+            pgui.click(duration=action_duration)
+            pgui.click(duration=action_duration)
