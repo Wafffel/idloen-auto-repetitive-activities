cf4b57acb25e49801ea30e10d699b150940a5b50
Wafffel <you@example.comkristoferski.kw@gmail.com>
2024-02-04 01:43:01 +0100

    add ballon usage

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import pyautogui as pgui
 from pynput import keyboard
 
 action_duration = 0.1
-hold_duration = 0.5
+hold_duration = 0.2
 pen_location = {'x':200, 'y':850}
 signature_location = {'x':200, 'y':770}
-items_location = {'x':200, 'y':850}
 candy_location = {'x':200, 'y':850}
-claim_location = {'x':200, 'y':770}
+ballon_location = {'x':200, 'y':770}
 stop_key = keyboard.Key.esc
 set_location_key = keyboard.Key.space
 
 sg.theme('Green')
-modes = ['silver pens', 'candies']
+modes = ['silver pens', 'candies', 'ballon']
 
 layout = [[sg.Combo(values=modes, default_value=modes[0], readonly=True), sg.Button(button_text='change mode', key='-change_mode-')],
           [sg.Text('to set position of object press the corresponding button')],
@@ -22,7 +21,8 @@ layout = [[sg.Combo(values=modes, default_value=modes[0], readonly=True), sg.But
           [sg.Button(button_text='set pens location', key='-set_pens_location-')],
           [sg.Button(button_text='set signature location', key='-set_signature_location-')],
           [sg.Button(button_text='set candy location', key='-set_candy_location-')],
-          [sg.Button(button_text='start', key='-start-'), sg.Text('press space to stop')]]
+          [sg.Button(button_text='set ballon location', key='-set_ballon_location-')],
+          [sg.Button(button_text='start', key='-start-'), sg.Text('press esc to stop')]]
 
 window = sg.Window(title='Idleon Hours Saver', layout=layout, element_justification='center', finalize=True)
 
@@ -40,6 +40,11 @@ def set_candy_location(key):
     if key == set_location_key:
         candy_location['x'], candy_location['y'] = pgui.position()
         return False
+    
+def set_ballon_location(key):
+    if key == set_location_key:
+        ballon_location['x'], ballon_location['y'] = pgui.position()
+        return False
 
 def stop(key):
     global is_running
@@ -48,6 +53,7 @@ def stop(key):
 
 is_running = False
 window['-set_candy_location-'].hide_row()
+window['-set_ballon_location-'].hide_row()
 
 with keyboard.Listener(on_press=stop) as listener:
     while True:
@@ -63,6 +69,9 @@ with keyboard.Listener(on_press=stop) as listener:
         if event == '-set_candy_location-':
             with keyboard.Listener(on_press=set_candy_location) as listener1:
                 listener1.join()
+        if event == '-set_ballon_location-':
+            with keyboard.Listener(on_press=set_ballon_location) as listener1:
+                listener1.join()                
         if event == '-start-':
             is_running = True
             if values[0] == modes[0]:
@@ -81,16 +90,25 @@ with keyboard.Listener(on_press=stop) as listener:
                     pgui.mouseUp(duration=action_duration)
                     time.sleep(action_duration)
                     pgui.press('space')
+            if values[0] == modes[2]:
+                while is_running:
+                    pgui.moveTo(ballon_location['x'], ballon_location['y'], action_duration)
+                    pgui.mouseDown(duration=action_duration)
+                    time.sleep(hold_duration)
+                    pgui.mouseUp(duration=action_duration)
         if event == '-change_mode-':
             window['-set_pens_location-'].hide_row()         
             window['-set_signature_location-'].hide_row()
             window['-set_candy_location-'].hide_row()
+            window['-set_ballon_location-'].hide_row()
             window['-start-'].hide_row()
             if values[0] == modes[0]:
                 window['-set_pens_location-'].unhide_row()
                 window['-set_signature_location-'].unhide_row()                
             if values[0] == modes[1]:
                 window['-set_candy_location-'].unhide_row()
+            if values[0] == modes[2]:
+                window['-set_ballon_location-'].unhide_row()
             window['-start-'].unhide_row()
         if event == sg.WIN_CLOSED:
             break
